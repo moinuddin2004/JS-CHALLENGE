844c7cd927aafe08c4990a622782e4de09ac6e38
moin uddin <s.m.moin2004@gmail.com>
2024-07-13 21:06:15 +0500

    day 1 done

---
+++ 1-day.js
@@ -0,0 +1,32 @@
+var num = 25
+let nam = "moin"
+console.log(num);
+console.log(nam);
+
+const flag = true
+console.log(flag);
+flag = 9;
+console.log(flag);
+
+let a = 1
+let b= "uddin"
+let c = true
+let d = {
+    name:"moin",
+    age: 21
+}
+let e = [1, 2, 3, 4, 5]
+
+// log each by typeof
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+
+let f = 7
+console.log(f);
+f = 8
+console.log(f);
+
